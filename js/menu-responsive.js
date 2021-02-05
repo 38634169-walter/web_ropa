8be4fb2e5f015b61f6b52e77556f115c841ca8a9
walter <walterdiaz9418@gmail.com>
2021-02-05 02:11:16 -0300

    menu-market

--- js/menu-responsive.js
+++ js/menu-responsive.js
@@ -20,32 +20,66 @@ function mostrar_menu(){
         }
     });
 
-    /*
-    $('.vestimentas-sub').click(function(){
-        $(this).children('.vestimentas-sub-li').slideToggle();
-        todo = 1;
-    });
-    
-    $('#vestimentas-li').click(function(){
-        if ( todo == 0){
-            $('.sub-menu').css({"display":"block"});
-            todo = 1;
-        }
-        if(todo == 2){
-            $('.sub-menu').css({"display":"none"});
-            todo = 0;
-        }
-    });
-    */ 
-    $('.dropbtn').click(function(){
-        $('.sub-menu2').removeClass('add-display');
-        $('.sub-menu').toggleClass('add-display');
-    });
-    $('.dropbtn2').click(function(){
-        $('.sub-menu2').toggleClass('add-display');
+    $('.dropbtn1').click(function(){
+        $('.sub-menu1').toggleClass('show');
+        $('.arrow-down-sub-menu1').toggleClass('remove');
+        $('.arrow-side-sub-menu1').toggleClass('show');
+
+        $('.sub-menu1-1').removeClass('show');
+        $('.sub-menu1-2').removeClass('show');
+        $('.sub-menu1-3').removeClass('show');
+        $('.sub-menu1-4').removeClass('show');
+        $('.sub-menu1-5').removeClass('show');
+        $('.sub-menu1-6').removeClass('show');
+
+        $('.down1').removeClass('remove');
+        $('.side1').removeClass('show');
+        $('.down2').removeClass('remove');
+        $('.side2').removeClass('show');
+        $('.down3').removeClass('remove');
+        $('.side3').removeClass('show');
+        $('.down4').removeClass('remove');
+        $('.side4').removeClass('show');
+        $('.down5').removeClass('remove');
+        $('.side5').removeClass('show');
+        $('.down6').removeClass('remove');
+        $('.side6').removeClass('show');
+
+
     });
 
 
+
+    $('.dropbtn1-1').click(function(){
+        $('.sub-menu1-1').toggleClass('show');
+        $('.down1').toggleClass('remove');
+        $('.side1').toggleClass('show');
+    });
+    $('.dropbtn1-2').click(function(){
+        $('.sub-menu1-2').toggleClass('show');
+        $('.down2').toggleClass('remove');
+        $('.side2').toggleClass('show');
+    });
+    $('.dropbtn1-3').click(function(){
+        $('.sub-menu1-3').toggleClass('show');
+        $('.down3').toggleClass('remove');
+        $('.side3').toggleClass('show');
+    });
+    $('.dropbtn1-4').click(function(){
+        $('.sub-menu1-4').toggleClass('show');
+        $('.down4').toggleClass('remove');
+        $('.side4').toggleClass('show');
+    });
+    $('.dropbtn1-5').click(function(){
+        $('.sub-menu1-5').toggleClass('show');
+        $('.down5').toggleClass('remove');
+        $('.side5').toggleClass('show');
+    });
+    $('.dropbtn1-6').click(function(){
+        $('.sub-menu1-6').toggleClass('show');
+        $('.down6').toggleClass('remove');
+        $('.side6').toggleClass('show');
+    });
 
 }
 
